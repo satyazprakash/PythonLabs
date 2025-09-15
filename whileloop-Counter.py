b54c1782a21f9e086faaff097d714b68783fe1f6
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-15 17:18:43 +0530

    Create whileloop-Counter.py

---
+++ whileloop-Counter.py
@@ -0,0 +1,10 @@
+# While loop Syntax with a count for variables
+#
+
+count = 0
+
+while count <= 10:
+    print(' Hello I am in while loop and count is : ', count)
+    count = count+1
+    count += 1
+
