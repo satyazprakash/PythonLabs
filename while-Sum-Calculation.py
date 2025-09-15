a7b315e4eb3331e7691f1f1be1cdb41c09748771
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-15 17:18:38 +0530

    Create while-Sum-Calculation.py

---
+++ while-Sum-Calculation.py
@@ -0,0 +1,16 @@
+#The following script calculates the sum of the numbers
+# from 1 to 100
+
+n = 100
+sum = 0
+
+counter = 1
+
+while  counter <= n:
+    sum = sum + counter
+    sum += counter
+
+print("Sum of 1 until 100 :  ",  sum )
+print('This is a sample print to test the program')
+
+    
