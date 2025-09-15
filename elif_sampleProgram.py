80a9933f3504da6c534dd53fc4b1941369c75cdc
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-15 17:18:19 +0530

    Create elif_sampleProgram.py

---
+++ elif_sampleProgram.py
@@ -0,0 +1,16 @@
+# Exampls of the Elif statement
+
+
+name = input('Please enter your Name : ')
+
+if name == 'Bryan':
+    print('That is a nice name')
+    
+elif name == 'John Hamilton':
+    print(' ... I am a Soccer Player ')
+
+elif name == 'Michael Palin':
+    print('..I am a  rugby Player ')
+    
+else:
+    print(' You have a nice name.  ')
