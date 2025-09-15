d48093259eace46756bd7b90f90025edd1c73ae2
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-15 17:18:32 +0530

    Create While-infinite-loop.py

---
+++ While-infinite-loop.py
@@ -0,0 +1,8 @@
+# The While Loop in python
+# The while infinite loop
+
+while 1:
+    print(' This is inside a while Loop ')
+    print('Type Ctrl +C to stop me and get out of this loop')
+    break
+    
