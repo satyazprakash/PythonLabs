587b0630b29b05d5e963411e5002d605cb0769b4
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-16 11:44:32 +0530

    Create if-Else-Sample-Int&inputfunction.py

---
+++ if-Else-Sample-Int&inputfunction.py
@@ -0,0 +1,15 @@
+# Additional  if Statement program
+# If Else Statement  nested
+
+marks = int( input(' Please Enter your Subject Marks Obtained :  '))
+
+if marks >=50:
+    print('Iff Success')  
+    print(' Congragulations  ')   #Statement 1
+    print('You cleared the exam') #Statement 2
+else:
+    print('Else Success')
+    print('You Failed')  #Statement 3
+    print('Better luck next time')  #Statement 4
+
+
