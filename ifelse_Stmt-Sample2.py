4dda3217b43d508f1a035b2d396ccdc24722b18b
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-16 11:44:46 +0530

    Create ifelse_Stmt-Sample2.py

---
+++ ifelse_Stmt-Sample2.py
@@ -0,0 +1,15 @@
+# this is a next if and Else statement
+x =6
+y =5
+
+if x==y :
+    print('X and Y are equal')
+
+else:
+    if x<y:
+        print('X is greater then Y')
+
+    else:
+        print('Y is greater then X')
+
+        
