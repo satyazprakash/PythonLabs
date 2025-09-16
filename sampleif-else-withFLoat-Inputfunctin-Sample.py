edc2e29b0c279816c3a4f37e72aacb8fccfa880e
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-16 11:44:52 +0530

    Create sampleif-else-withFLoat-Inputfunctin-Sample.py

---
+++ sampleif-else-withFLoat-Inputfunctin-Sample.py
@@ -0,0 +1,12 @@
+# if else syntax sample 3 program.
+
+weight = float(input(' How many KG does your Suitcase weigh :  '))
+
+
+if weight >50 :
+    print('There is a 25 INR charge for luggage that heavy')
+    print('Thank you for your Business')
+
+else:
+    print('Have a safe Journey')
+    
