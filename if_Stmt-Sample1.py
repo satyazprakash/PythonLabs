87158bbdb41be13d1def8ffdbcfcc065b910afb3
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-16 11:44:40 +0530

    Create if_Stmt-Sample1.py

---
+++ if_Stmt-Sample1.py
@@ -0,0 +1,22 @@
+# If SAtatement program to check  logic.
+
+x = 0
+
+if x==0:
+    print('X is a positive Number')
+
+
+y=-1
+    
+if  y<= 0 :
+    print('Y is less then Zero')
+
+    
+z=int(input('Enter the value for number Z to check for Odd or even'))
+
+if z%2==0:
+    print('Z is an even Number')
+
+else:
+    Print('Z is an Odd Number')
+    
