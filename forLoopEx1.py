8005a04b26f4f33e67dc6ff0faa462854b4d3c3e
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-17 17:38:18 +0530

    Create forLoopEx1.py

---
+++ forLoopEx1.py
@@ -0,0 +1,34 @@
+# program for Loop
+
+words = ['cat', 'dog','window','i like python']
+
+for w in words:
+    print(w, len(w))
+
+## this is another example
+
+primes =[1,2,3,5,7,11]
+for prime in primes :
+    print(prime)
+
+# this is another example
+
+for count in (1,2,3,4,5):
+    print("down")
+    print("up")
+    print(count)
+    print("yes" * count )
+    
+# this is the third example
+for  count in (1,2,3,4,5,6):
+    print("down")
+    print("up")
+    print(count)
+    print("yes" * count )
+    print('done counting')
+for color in ['red','green','orange']:
+    print(color)
+    
+
+
+    
