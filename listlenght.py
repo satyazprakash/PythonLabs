56daa23d9b7215256f6a229726b65955a11faafc
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-17 17:38:22 +0530

    Create listlenght.py

---
+++ listlenght.py
@@ -0,0 +1,11 @@
+# using operators in Lists
+
+list1 = [123, 'banaras', 'pune', 'new jersey','new delhi']
+list2 = [456, 'abc', 'xyz', 'efg']
+
+print(" First list lenght :", len(list1))
+print("Second list length: ", len(list2))
+
+print(list1 + list2)
+
+print(list1 * 5 )
