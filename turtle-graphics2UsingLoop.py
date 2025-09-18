fd0ca8de98352982d37ec1f500b0f1ae26ff70f7
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-18 12:11:59 +0530

    Create turtle-graphics2UsingLoop.py

---
+++ turtle-graphics2UsingLoop.py
@@ -0,0 +1,15 @@
+# Graphics Program using Turtle Graphics.
+
+import  turtle
+
+alex=turtle.Turtle()
+
+for i in 0,1,2,3 : 
+    alex.forward(250)
+    alex.left(90)
+    
+
+
+for i in 0,1,2 : 
+    alex.forward(250)
+    alex.left(120)
