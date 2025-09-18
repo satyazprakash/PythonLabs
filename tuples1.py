67bb2d25f02456351f8e43eee0455bd106e96c9b
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-18 12:12:21 +0530

    Create tuples1.py

---
+++ tuples1.py
@@ -0,0 +1,16 @@
+#Tuples in Python
+
+tup1 = ('physics','12345','satya','biology','myhome')
+tup2 = (1,2,3,4,5,6,7,8,9,0)
+
+print('this prints tup[0] : ', tup1[0])
+print('this prints tup2[1:5] : ', tup2[1:5])
+
+''' Tuples are immutalbe which means we cannnot
+update or change values of tuples elements
+we can change the values of tuple elements to create a
+new tumpe '''
+
+tup1 =(12, 34, 56);
+tup2 =('abc', 'xyz');
+
