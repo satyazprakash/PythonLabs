190914233409b7546ef4d10d9adb223b572acf00
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-20 18:10:55 +0530

    Create myInputFunction.py

---
+++ myInputFunction.py
@@ -0,0 +1,8 @@
+# The following will ask user for username
+name = input("What 's your name?")
+print("Nice to meet you " + name + "!")
+age = input("Your is your age ? ")
+print("So, you are already " +  age  + " years old, " + name + " !")
+color = input("What is your favourite color ? "  )
+
+print("So your favourite color is  : "  + color + " !" )
