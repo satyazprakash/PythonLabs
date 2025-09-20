c26c9fec2aed565394b823324b5657aa6658d57b
Satya Prakash <sprakash.satya1@gmail.com>
2025-09-20 18:10:44 +0530

    Create myPrintme_Function.py

---
+++ myPrintme_Function.py
@@ -0,0 +1,15 @@
+# Function definition is here
+
+def printme (str):
+    "This prints a passed string into this function"
+    print(str)
+    return;
+
+# Now we can call the printme function
+
+print("I am first call to user defined function")
+print("Again second call to the same function")
+
+
+
+
